825f56abdcdcdeb7e017c4034e1f5be007ef9147
zhangtao615 <295564283@qq.com>
2020-11-19 21:34:36 +0800

    '获取网页内容'

--- src/crowller.ts
+++ src/crowller.ts
@@ -1,17 +1,38 @@
-import superagent from 'superagent'
-import cheerio from 'cheerio'
+import superagent from 'superagent';
+import cheerio from 'cheerio';
+interface Course {
+  title: string;
+  count: number;
+}
 class Crowller {
-  private url = `http://es.xiecheng.live/`
+  private courseInfo: Course[] = []
+  private secret = 'x3b174jsx';
+  private url = `http://www.dell-lee.com/typescript/demo.html?secret=${this.secret}`;
   async getRawHtml () {
-    const result = await superagent.get(this.url);
-    this.getInfo(result.text);
+   const res = await superagent.get(this.url);
+   this.getCourseInfo(res.text)
   }
-  getInfo (html: string) {
-    const $ = cheerio.load(html)
-  }
+  getCourseInfo (html: string) {
+    const $ = cheerio.load(html);
+    const courseItems = $('.course-item');
+    console.log(courseItems.length)
+    courseItems.map((index, item) => {
+      const descs = $(item).find('.course-desc')
+      const title = descs.eq(0).text();
+      const count = parseInt(descs.eq(1).text().split('：')[1], 10);
+      this.courseInfo.push({
+        title, count
+      });
+    });
+    const res = {
+      time: (new Date()).getTime(),
+      data: this.courseInfo
+    };
+    console.log(res)
+  };
   constructor () {
-    this.getRawHtml()
+    this.getRawHtml();
   }
 }
 
-const crowller  = new Crowller()
+const crowller  = new Crowller();
